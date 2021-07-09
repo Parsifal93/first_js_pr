365de4dd7bee51e6aabd9e4b717648a657ccb05c
VadymStratiienko <vadimstratiienko@gmail.com>
2021-07-09 20:44:11 +0300

    first js

---
+++ index.js
@@ -0,0 +1,22 @@
+// let message='22';
+// let user='User';
+// let num=2;
+
+
+// alert(num);
+// alert(user);
+// alert(message); 
+// alert(num+user);
+// alert(message+num);
+
+let num=22;
+let str='text';
+let num2=true;
+let num3=false;
+let srtoc='111';
+let num4=222;
+
+console.log(Number (srtoc)+num4);
+console.log(num2+num3);
+console.log(num3-num2);
+console.log(Number(num+str));
