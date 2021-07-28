9f40f63aa151fba2145023ceea2c664ae38054d2
VadymStratiienko <vadimstratiienko@gmail.com>
2021-07-28 19:37:44 +0300

    add test 1-10

--- index2.js
+++ index2.js
@@ -20,16 +20,70 @@
 // console.log(arrayStr);
 
 /////1////
-const numbers = [1, 2,3,5,6,7,8,9];
-console.log(numbers.filter(function(value) {
-    return value % 2 ===1;
-}));
+// const numbers = [1, 2,3,5,6,7,8,9];
+// console.log(numbers.filter(function(value) {
+//     return value % 2 ===1;
+// }));
 
 
-///////2///////
+// ///////2///////
 
-const strings = ["Игнат", "Валерия", "Игорь", "Александр"];
+// const strings = ["Игнат", "Валерия", "Игорь", "Александр"];
 
-console.log(strings.filter(function (value){
-    return value.charAt(0) === "И"
-}));
+// console.log(strings.filter(function (value){
+//     return value.charAt(0) === "И"
+// }));
+
+
+//////первое задание///////
+// const array = [];
+// const num = array.push (1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+// console.log(array);
+// console.log(num);
+
+
+//////второе задание/////
+// const array = [1, 2, 3, 4, 5, 6];
+
+// console.log(array.unshift(0));
+// console.log(array);
+
+// //////третье задание////////
+// const array = [1, 2, 3, 4, 5, 6];
+// array.pop()
+// array.shift()
+
+// console.log(array);
+
+
+//////четвернтое/пятое задание////////
+// const array = [1, 2, 3, 4, 5, 6];
+// const remov = array.splice(3,1, 9);
+
+// console.log(array);
+
+////////шестое/седьмое задание/////////
+// const array = [1, 2, 3, 4, 5, 6];
+// const clone = Object.assign([],array);
+// if (clone.length>3){
+//     clone.length = 3;
+// }
+
+// console.log(array);
+// console.log(clone);
+
+//////восьмое задание///////
+// const  array = [[],[],[],[],[],[],[],[]];
+
+// console.log(array);
+
+///////девятое задание///////
+// const  array = [[],[],[],[],[],[],[],[]];
+
+// console.log(array.fill(0,0,4));
+
+
+///////десятое задание////////
+const array = [1,2,3,4,5,6,7,8,9,10];
+
